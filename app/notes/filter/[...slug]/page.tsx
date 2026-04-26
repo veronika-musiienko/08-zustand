892c36d3f756607885ed611a89ae367294060951
Veronika <mnika5437@gmail.com>
2026-04-26 23:18:25 +0300

    fix pg

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,46 +1,35 @@
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
-import type { NoteSearchResponse } from "@/lib/api";
-import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ slug?: string[] }>;
 };
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const tag = (await params).slug?.[0] || "All";
-  const selectedTag = tag === "All" ? "All" : tag;
-
-  return {
-    title: `Notes tag: ${selectedTag} | NoteHub`,
-    description: `Browse all notes tagged with "${selectedTag}" on NoteHub. Discover and manage notes related to this topic.`,
-    openGraph: {
-      title: `Notes tag: ${selectedTag} | NoteHub`,
-      description: `View and explore notes marked with the tag "${selectedTag}". Find all your content related to this category in one place.`,
-      url: `https://notehub.com/notes/filter/${selectedTag}`,
-      siteName: "NoteHub",
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: `Notes tagged with ${selectedTag}`,
-        },
-      ],
-      type: "website",
-    },
-  };
-}
 
 export default async function NotesPage({ params }: Props) {
   const paramsQuery = await params;
-  const [tag] = paramsQuery.slug || [];
+  const slug = paramsQuery.slug || [];
+  const tag = slug[0] || "all";
   const page = 1;
 
-  const initialData: NoteSearchResponse = await fetchNotes({
-    tag,
-    searchQuery: "",
-    page,
+  const queryClient = new QueryClient();
+
+  // 1. Попередньо завантажуємо дані в кеш
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", "", tag, page], 
+    queryFn: () =>
+      fetchNotes({
+        searchQuery: "",
+        tag: tag.toLowerCase() === "all" ? undefined : tag,
+        page: page,
+      }),
   });
 
-  return <NotesClient initialData={initialData} tag={tag} />;
+  return (
+    // 2. Обгортаємо в HydrationBoundary
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {/* 3. ВИДАЛЯЄМО initialData! Передаємо тільки tag */}
+      <NotesClient tag={tag} />
+    </HydrationBoundary>
+  );
 }
